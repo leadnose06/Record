b590584d1865a201f70850e3cb5fab4c2fe62dc5
Jackson <leadnose06@gmail.com>
2024-10-08 11:39:45 -0700

    working on grapple

--- Assets/Scripts/GrapplingScript.cs
+++ Assets/Scripts/GrapplingScript.cs
@@ -7,6 +7,8 @@ public class GrapplingScript : MonoBehaviour
 {
     public GameObject objectTracker;
     private GameObject[] grappleables;
+    private Transform[] LegalTargets;
+    public bool IsGrappling = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,17 @@ public class GrapplingScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(!IsGrappling){
+            grappleables = objectTracker.GetComponent<ObjectTrackerScript>().enemies.Concat(objectTracker.GetComponent<ObjectTrackerScript>().grapples).ToArray();
+            foreach(GameObject a in grappleables){
+                RaycastHit2D grappleCheck = Physics2D.Raycast(transform.position, a.transform.position - transform.position, 100f);
+                Debug.Log(grappleCheck.distance);
+                Debug.DrawRay(transform.position, a.transform.position, Color.black, 10f);
+            }
+
+        }
+
     }
 }
