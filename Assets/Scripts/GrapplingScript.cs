a419a11dcc4f42f60729441e58be603e5a8583ba
Jackson <leadnose06@gmail.com>
2024-10-10 11:41:23 -0700

    working on grapple

--- Assets/Scripts/GrapplingScript.cs
+++ Assets/Scripts/GrapplingScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GrapplingScript : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class GrapplingScript : MonoBehaviour
     private GameObject[] grappleables;
     private Transform[] LegalTargets;
     public bool IsGrappling = false;
+
+    [SerializeField]private float minMax = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +25,33 @@ public class GrapplingScript : MonoBehaviour
     void FixedUpdate()
     {
         if(!IsGrappling){
+            var gamepad = Gamepad.current;
+            Vector2 move = gamepad.rightStick.ReadValue();
             grappleables = objectTracker.GetComponent<ObjectTrackerScript>().enemies.Concat(objectTracker.GetComponent<ObjectTrackerScript>().grapples).ToArray();
-            foreach(GameObject a in grappleables){
-                RaycastHit2D grappleCheck = Physics2D.Raycast(transform.position, a.transform.position - transform.position, 100f);
-                Debug.Log(grappleCheck.distance);
-                Debug.DrawRay(transform.position, a.transform.position, Color.black, 10f);
+            LegalTargets = new Transform[0];
+            if(Mathf.Abs(move.x) > 0.001 || Mathf.Abs(move.y) > 0.001){
+                float JoystickAngle = Mathf.Rad2Deg*Mathf.Atan(move.y/move.x);
+                float min = (JoystickAngle - minMax + 360)%360;
+                float max = (JoystickAngle + minMax)%360;
+                foreach(GameObject a in grappleables){
+                    RaycastHit2D grappleCheck = Physics2D.Raycast(transform.position, a.transform.position - transform.position, 100f);
+                    float TargetAngle = Mathf.Rad2Deg * Mathf.Atan(grappleCheck.transform.position.y/grappleCheck.transform.position.x);
+                    Debug.Log(grappleCheck.distance);
+                    Debug.DrawRay(transform.position, a.transform.position);
+                    if(grappleCheck.collider.tag == "Grappleable" || grappleCheck.collider.tag == "Enemy" ){
+                        if(max + 180 < min) {
+                            if(TargetAngle > min || TargetAngle < max){
+                                LegalTargets.Append(grappleCheck.transform);
+                            }
+                        }
+                        else{
+                            if(TargetAngle > min && TargetAngle < max){
+                                LegalTargets.Append(grappleCheck.transform);
+                            }
+                        }
+                    }
+                }
             }
-
         }
 
     }
