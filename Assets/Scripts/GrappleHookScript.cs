c409822c6cc5d5ca208b71558ece3cf5efaaba8c
Jackson <leadnose06@gmail.com>
2024-10-18 11:37:33 -0700

    Fixed Grapple Targeting

--- Assets/Scripts/GrappleHookScript.cs
+++ Assets/Scripts/GrappleHookScript.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class GrappleHookScript : MonoBehaviour
 {
     public GameObject player;
-    //public Vector3 targetPos;
+    public Vector3 targetPos;
     public float targetDir;
-    public float speed = 20f;
+    public Vector3 Direction;
+    public float speed = 3f;
+    public bool target;
+    Rigidbody2D triggerrb;
     void Awake()
     {
-        //gameObject.SetActive(true);
-        //targetDir = (targetPos - transform.position).normalized;
+        Direction = (targetPos - transform.position).normalized;
+        triggerrb = GetComponent<Rigidbody2D>();
+        if(!target){
+            triggerrb.velocity = new Vector2(Mathf.Cos(Mathf.Deg2Rad*targetDir), Mathf.Sin(Mathf.Deg2Rad*targetDir)) * speed;
+            //triggerrb.velocity = new Vector2(5, 5) * speed;
+        } else{
+            triggerrb.velocity = Direction * speed;
+
+        }
     }
 
     void Update()
-    {
-        transform.position += new Vector3(Mathf.Cos(Mathf.Deg2Rad*targetDir), Mathf.Sin(Mathf.Deg2Rad*targetDir)) * speed * Time.deltaTime;
+    {/*
+        if(!target){
+            transform.position += new Vector3(Mathf.Cos(Mathf.Deg2Rad*targetDir), Mathf.Sin(Mathf.Deg2Rad*targetDir)) * speed * Time.deltaTime;
+            triggerrb.velocity = new Vector2(Mathf.Cos(Mathf.Deg2Rad*targetDir), Mathf.Sin(Mathf.Deg2Rad*targetDir)) * speed;
+        } else{
+            transform.position += Direction * speed * Time.deltaTime;
+
+        }*/
     }
 
-    public void OnCollisionEnter2D(Collision2D col){
-        if(col.collider.tag != "Player"){
-            if(col.collider.tag == "Grappleable" || col.collider.tag == "Enemy"){
+    public void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if(collision.tag != "Player"){
+            if(collision.tag == "Grappleable" || collision.tag == "Enemy"){
 
             }else{
                 Destroy(gameObject);
--- Assets/Scripts/GrapplingScript.cs
+++ Assets/Scripts/GrapplingScript.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
 
 public class GrapplingScript : MonoBehaviour
 {
     public GameObject objectTracker;
     private GameObject[] grappleables;
-    private Transform[] LegalTargets;
-    private float[] LegalAngleDiffs;
+    private List<Transform> LegalTargets;
+    private List<float> LegalAngleDiffs;
     public bool IsGrappling = false;
     public GameObject grappleHookPattern;
     public GameObject grappleHook;
+    private float grappleCooldown;
 
-    [SerializeField]private float minMax = 10f;
+    private float minMax = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +27,13 @@ public class GrapplingScript : MonoBehaviour
         grappleables = objectTracker.GetComponent<ObjectTrackerScript>().enemies.Concat(objectTracker.GetComponent<ObjectTrackerScript>().grapples).ToArray();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        
+        if(IsGrappling){
+            if(Time.time >= grappleCooldown+1.5f){
+                IsGrappling = false;
+            }
+        }
 
     }
     public void onFire(InputAction.CallbackContext context){
@@ -36,40 +41,49 @@ public class GrapplingScript : MonoBehaviour
             var gamepad = Gamepad.current;
             Vector2 move = gamepad.rightStick.ReadValue();
             grappleables = objectTracker.GetComponent<ObjectTrackerScript>().enemies.Concat(objectTracker.GetComponent<ObjectTrackerScript>().grapples).ToArray();
-            LegalTargets = new Transform[0];
-            LegalAngleDiffs = new float[0];
+            LegalTargets = new List<Transform>();
+            LegalAngleDiffs = new List<float>();
             if(Mathf.Abs(move.x) > 0.001f || Mathf.Abs(move.y) > 0.001f){
-                float JoystickAngle = Mathf.Rad2Deg*Mathf.Atan(move.y/move.x);
-                /*float min = (JoystickAngle - minMax + 360)%360;
+                float JoystickAngle = (Mathf.Rad2Deg*Mathf.Atan2(move.y, move.x)+360)%360;
+                //
+                float min = (JoystickAngle - minMax + 360)%360;
                 float max = (JoystickAngle + minMax)%360;
                 foreach(GameObject a in grappleables){
                     RaycastHit2D grappleCheck = Physics2D.Raycast(transform.position, a.transform.position - transform.position, 100f);
-                    float TargetAngle = Mathf.Rad2Deg * Mathf.Atan(grappleCheck.transform.position.y/grappleCheck.transform.position.x);
-                    Debug.Log(grappleCheck.distance);
-                    Debug.DrawRay(transform.position, a.transform.position);
+                    float TargetAngle = (Mathf.Rad2Deg * Mathf.Atan2(grappleCheck.transform.position.y, grappleCheck.transform.position.x)+360)%360;
                     if(grappleCheck.collider.tag == "Grappleable" || grappleCheck.collider.tag == "Enemy" ){
                         if(max + 180 < min) {
                             if(TargetAngle > min || TargetAngle < max){
-                                LegalTargets.Append(grappleCheck.transform);
-                                LegalAngleDiffs.Append(Mathf.Abs(((JoystickAngle+180)%360)-((TargetAngle+180)%360)));
+                                LegalTargets.Add(grappleCheck.transform);
+                                LegalAngleDiffs.Add(Mathf.Abs(((JoystickAngle+180)%360)-((TargetAngle+180)%360)));
                             }
                         }
                         else{
                             if(TargetAngle > min && TargetAngle < max){
-                                LegalTargets.Append(grappleCheck.transform);
-                                LegalAngleDiffs.Append(Mathf.Abs(JoystickAngle-TargetAngle));
+                                LegalTargets.Add(grappleCheck.transform);
+                                LegalAngleDiffs.Add(Mathf.Abs(JoystickAngle-TargetAngle));
                             }
                         }
                     }
-                }*/
+                }//
                         
                 grappleHook = Instantiate(grappleHookPattern);
-                grappleHook.transform.position = transform.position;
+                grappleHook.transform.position = new Vector3(transform.position.x, transform.position.y);
                 grappleHook.GetComponent<GrappleHookScript>().player = gameObject;
-                //grappleHook.GetComponent<GrappleHookScript>().targetPos = LegalTargets[Array.IndexOf(LegalAngleDiffs, LegalAngleDiffs.Min())].position;
-                grappleHook.GetComponent<GrappleHookScript>().targetDir = JoystickAngle;
+                Debug.Log("Angle: "+ JoystickAngle);
+                Debug.Log("min: "+ min);
+                Debug.Log("max: "+ max);
+                //Debug.Log(LegalTargets[0].position.x);
+                if(LegalTargets.Count > 0 && LegalAngleDiffs.Count > 0){
+                    grappleHook.GetComponent<GrappleHookScript>().targetPos = LegalTargets[LegalAngleDiffs.IndexOf(LegalAngleDiffs.Min())].position;
+                    grappleHook.GetComponent<GrappleHookScript>().target = true;
+                }else{
+                    grappleHook.GetComponent<GrappleHookScript>().targetDir = JoystickAngle;
+                    grappleHook.GetComponent<GrappleHookScript>().target = false;
+                }
                 grappleHook.SetActive(true);
                 IsGrappling = true;
+                grappleCooldown = Time.time;
             }
 
 
