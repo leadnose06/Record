2a5ae64970de60d50022f8ab67b477cb21cd780a
Jackson <leadnose06@gmail.com>
2024-10-22 10:33:37 -0700

    Working on movement

--- Assets/Scripts/TouchingDirections.cs
+++ Assets/Scripts/TouchingDirections.cs
@@ -13,11 +13,25 @@ public class TouchingDirections : MonoBehaviour
     public float groundDistance = 0.05f;
     [SerializeField]
     private bool _isGrounded;
+    private bool _isOnWall;
+    private bool _isOnCeiling;
+    private Vector2 wallCheckDirection;
 
     public bool isGrounded { get{
         return _isGrounded;
     } private set{
         _isGrounded = value;
+    } }
+
+    public bool isOnWall { get{
+        return _isOnWall;
+    } private set{
+        _isOnWall = value;
+
+    } }public bool isOnCeiling { get{
+        return _isOnCeiling;
+    } private set{
+        _isOnCeiling = value;
     } }
     
     
@@ -29,6 +43,8 @@ public class TouchingDirections : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
+        //isOnWall = touchingCol.Cast(wallCheckDirection, castFilter, wallHits, wallDistance) > 0;
+        //isOnCeiling = touchingCol.Cast(Vector2.up, castFilter, ceilingHits, ceilingDistance) > 0;
     }
 
 
