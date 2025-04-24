9ec7952e1f8223c9e8a5a6bbb5dc392a2ce4b184
Jackson <leadnose06@gmail.com>
2025-04-24 11:29:19 -0700

    made abilities collectible

--- Assets/Scripts/UI Controls/Description Manager.cs
+++ Assets/Scripts/UI Controls/Description Manager.cs
@@ -32,7 +32,7 @@ public class DescriptionManager : MonoBehaviour
         descriptions[0] = "The remaining strength of your armor. If it runs out, you succumb to the elements";
         descriptions[1] = "Nanobots that can be used to partially repair your armor. You only have a limited amount, use them wisely";
         descriptions[2] = "A small horizontal boost. Use it to evade enemies and cross large gaps. It has a cooldown of half a second after use";
-        descriptions[3] = "Your grappling hook. It can be used to gain a great deal of height by grappling onto enemies and grapple points. It takes a second to recharge afte using it.";
+        descriptions[3] = "Your grappling hook. It can be used to gain a lot of height by grappling onto enemies and grapple points. It takes a second to recharge afte using it.";
         descriptions[4] = "Use your double jump to reach high ledges and jump over enemies. If used alongside your other abilities, it can let you stay airborne for a very long time.";
         #endregion
     }
@@ -45,10 +45,21 @@ public class DescriptionManager : MonoBehaviour
 
     void Update()
     {
-        if(ArrayUtility.IndexOf(objects, EventSystem.current.currentSelectedGameObject) != lastIndex){
-            nameDisplay.text = names[ArrayUtility.IndexOf(objects, EventSystem.current.currentSelectedGameObject)];
-            descriptionDisplay.text = descriptions[ArrayUtility.IndexOf(objects, EventSystem.current.currentSelectedGameObject)];
-            lastIndex = ArrayUtility.IndexOf(objects, EventSystem.current.currentSelectedGameObject);
+        int index = ArrayUtility.IndexOf(objects, EventSystem.current.currentSelectedGameObject);
+        if(index != lastIndex){
+            if(index <= 1){
+                nameDisplay.text = names[index];
+                descriptionDisplay.text = descriptions[index];
+                lastIndex = index;
+            } else if((index == 2 && DataManager.Instance.playerData.dash) || (index == 3 && DataManager.Instance.playerData.grapple) || (index == 4 && DataManager.Instance.playerData.doubleJump)){
+                nameDisplay.text = names[index];
+                descriptionDisplay.text = descriptions[index];
+                lastIndex = index;
+            } else{
+                nameDisplay.text = "Not Collected";
+                descriptionDisplay.text = "You have not found this item yet";
+                lastIndex = index;
+            }
         }
     }
 }
