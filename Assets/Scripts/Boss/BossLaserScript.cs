30ce18b7e321d4db1a00ef58946003c8894ff528
Jackson <leadnose06@gmail.com>
2025-05-20 10:48:45 -0700

    almost finished with boss

--- Assets/Scripts/Boss/BossLaserScript.cs
+++ Assets/Scripts/Boss/BossLaserScript.cs
@@ -9,37 +9,51 @@ public class BossLaserScript : MonoBehaviour
     public bool clockwise;
     private float previewDuration;
     public float turnSpeed;
+    public GameObject player;
 
     void Start()
     {
-        
+
     }
     void OnEnable()
     {
-        gameObject.transform.localScale = new Vector3(100, 0.15f, 1);
-        previewDuration = 0.45f;
+        gameObject.transform.localScale = new Vector3(0.15f, 100f, 1);
+        previewDuration = 0.75f;
         GetComponent<Collider2D>().enabled = false;
     }
 
     void Update()
     {
         previewDuration -= Time.deltaTime;
-        if(previewDuration <= 0f){
+        if (previewDuration <= 0f)
+        {
             duration -= Time.deltaTime;
         }
-        if(duration <= 0){
+        if (duration <= 0)
+        {
             gameObject.SetActive(false);
         }
-        if(previewDuration <= 0 &! GetComponent<Collider2D>().enabled){
-            gameObject.transform.localScale = new Vector3(100, 0.75f, 1);
+        if (previewDuration <= 0 & !GetComponent<Collider2D>().enabled)
+        {
+            gameObject.transform.localScale = new Vector3(0.75f, 100f, 1);
             GetComponent<Collider2D>().enabled = true;
         }
-        if(previewDuration <= 0 && spin){
-            if(clockwise){
-                transform.Rotate(0, 0, -turnSpeed * Time.deltaTime);
-            }else{
+        if (previewDuration <= 0 && spin)
+        {
+            if (clockwise)
+            {
                 transform.Rotate(0, 0, -turnSpeed * Time.deltaTime);
             }
+            else
+            {
+                transform.Rotate(0, 0, -turnSpeed * Time.deltaTime);
+            }
+        }
+    }
+        void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player" && !DataManager.Instance.invulnerable){
+            player.GetComponent<PlayerController>().damage(1);
+            player.transform.Find("EnemyCollider").gameObject.GetComponent<PlayerColliderScript>().SetInvulnerable();
         }
     }
 
--- Assets/Scripts/Boss/BossScript.cs
+++ Assets/Scripts/Boss/BossScript.cs
@@ -17,40 +17,76 @@ public class BossScript : MonoBehaviour
     public bool activated;
     public int attackChoice;
     public GameObject player;
+    public Sprite flyingBoss;
 
     void Start()
     {
         phase = 1;
         activated = true;
-        laserArray = new GameObject[6];
-        bulletArray = new GameObject[20];
-        for(int i=0; i<6; i++){
+        laserArray = new GameObject[15];
+        bulletArray = new GameObject[50];
+        for (int i = 0; i < 15; i++)
+        {
             laserArray[i] = Instantiate(laserPattern);
+            laserArray[i].GetComponent<BossLaserScript>().player = player;
             laserArray[i].SetActive(false);
         }
-        for(int i=0; i<20; i++){
+        for (int i = 0; i < 50; i++)
+        {
             bulletArray[i] = Instantiate(bulletPattern);
             bulletArray[i].SetActive(false);
         }
-        CurrentHealth = 90f;
+        CurrentHealth = 15f;
     }
 
     void Update()
     {
-        if (colorIsRed && colorHitTimer <= 0) {sprite.color = Color.white;}
-        else {colorHitTimer -= Time.deltaTime;}
+        if (phase == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(46, 24), 4*Time.deltaTime);
+        }
+        if (colorIsRed && colorHitTimer <= 0) { sprite.color = Color.white; }
+        else { colorHitTimer -= Time.deltaTime; }
         attackCooldown -= Time.deltaTime;
-        if(activated){
-            if(attackCooldown <= 0){
-                if(phase == 1){
-                    attackChoice = Random.Range(0, 4);
-                    for(int i=0; i<laserArray.Length; i++){
-                        if(laserArray[i].activeSelf == false){
-                            laserTarget(i);  
-                        }
+        if (activated)
+        {
+            if (attackCooldown <= 0)
+            {
+                if (phase == 1)
+                {
+                    attackChoice = Random.Range(0, 2);
+                    if (attackChoice == 0)
+                    {
+
+                        LaserTarget();
+
                     }
-                    attackCooldown = 4f;
+                    else
+                    {
+                        LaserRows(1.1f);
+                    }
                 }
+                else
+                {
+                    attackChoice = Random.Range(0, 3);
+                    switch (attackChoice)
+                    {
+                        case 0:
+                            LaserTarget();
+                            Invoke("LaserTarget", 0.7f);
+                            Invoke("LaserTarget", 1.4f);
+                            break;
+                        case 1:
+                            LaserRows(2f);
+                            break;
+                        case 2:
+                            BulletCircle();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                attackCooldown = 4f;
             }
         }
 
@@ -61,30 +97,83 @@ public class BossScript : MonoBehaviour
         colorIsRed = true;
         colorHitTimer = 0.1f;
         CurrentHealth -= damageAmount;
-        if(CurrentHealth <= 0f){
-            if(phase == 1){
+        if (CurrentHealth <= 0f)
+        {
+            if (phase == 1)
+            {
                 phase = 2;
                 NextPhase();
-            }else{
+                attackCooldown = 7f;
+            }
+            else
+            {
                 this.enabled = false;
+                gameObject.SetActive(false);
             }
         }
     }
-    public void Activate(){
+    public void Activate()
+    {
         activated = true;
         attackCooldown = 0.6f;
     }
 
-    private void NextPhase(){
-
+    private void NextPhase()
+    {
+        GetComponent<SpriteRenderer>().sprite = flyingBoss;
+        for (int i = 0; i < laserArray.Length; i++)
+        {
+            laserArray[i].SetActive(false);
+        }
+        CurrentHealth = 50;
     }
 
-    private void laserTarget(int index){
-        laserArray[index].transform.position = transform.position;
-        laserArray[index].transform.eulerAngles = new Vector3(0, 0, Mathf.Rad2Deg*Mathf.Atan2(player.transform.position.y-transform.position.y, player.transform.position.x-transform.position.x));
-        laserArray[index].GetComponent<BossLaserScript>().duration = 0.9f;
+    private void LaserTarget()
+    {
+        int index = 0;
+        for (int i = 0; i < laserArray.Length; i++)
+        {
+            if (laserArray[i].activeSelf == false)
+            {
+                index = i;
+                break;
+            }
+        }
+        laserArray[index].transform.eulerAngles = new Vector3(0, 0, 0);
+        laserArray[index].transform.position = new Vector3(transform.position.x, transform.position.y + 0.75f, 0);
+        laserArray[index].transform.RotateAround(transform.position + new Vector3(0, 0.75f, 0), new Vector3(0, 0, 1), Mathf.Rad2Deg * Mathf.Atan2(player.transform.position.y - (transform.position.y + 0.75f), player.transform.position.x - transform.position.x) - 90f);
+        //laserArray[index].transform.RotateAround(new Vector3(transform.position.x, transform.position.y+2f, 0f), new Vector3(0,0,1), Mathf.Rad2Deg*Mathf.Atan2(player.transform.position.y-transform.position.y, player.transform.position.x-transform.position.x));
+        laserArray[index].GetComponent<BossLaserScript>().duration = 0.6f;
         laserArray[index].GetComponent<BossLaserScript>().spin = false;
         laserArray[index].SetActive(true);
+    }
+
+    private void LaserRows(float width)
+    {
+        for (int i = 1; i < 7; i += 2)
+        {
+            laserArray[i].transform.eulerAngles = new Vector3(0, 0, 0);
+            laserArray[i].transform.position = new Vector3(transform.position.x - width * i, transform.position.y - 30f, 0);
+            laserArray[i].GetComponent<BossLaserScript>().duration = 0.6f;
+            laserArray[i].GetComponent<BossLaserScript>().spin = false;
+            laserArray[i].SetActive(true);
+
+            laserArray[i + 1].transform.eulerAngles = new Vector3(0, 0, 0);
+            laserArray[i + 1].transform.position = new Vector3(transform.position.x + width * i, transform.position.y - 30f, 0);
+            laserArray[i + 1].GetComponent<BossLaserScript>().duration = 0.6f;
+            laserArray[i + 1].GetComponent<BossLaserScript>().spin = false;
+            laserArray[i + 1].SetActive(true);
+        }
+    }
+    private void BulletCircle()
+    {
+        for (int i = 0; i < 45; i++)
+        {
+            float angle = i * 8f;
+            bulletArray[i].GetComponent<BossBulletScripe>().direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0);
+            bulletArray[i].transform.position = transform.position + new Vector3(0, 0.5f, 0);
+            bulletArray[i].SetActive(true);
+        }
     }
 
 }
