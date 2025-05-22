0c48200e82e699d9f2ceb59bfb624436227eef76
Jackson <leadnose06@gmail.com>
2025-05-22 11:35:35 -0700

    finished boss room

--- Assets/Scripts/Boss/BossScript.cs
+++ Assets/Scripts/Boss/BossScript.cs
@@ -18,25 +18,29 @@ public class BossScript : MonoBehaviour
     public int attackChoice;
     public GameObject player;
     public Sprite flyingBoss;
+    private float laserTargetCooldown;
+    private int laserQueue;
 
     void Start()
     {
         phase = 1;
         //activated = true;
-        laserArray = new GameObject[15];
-        bulletArray = new GameObject[50];
-        for (int i = 0; i < 15; i++)
+        laserArray = new GameObject[20];
+        bulletArray = new GameObject[100];
+        for (int i = 0; i < 20; i++)
         {
             laserArray[i] = Instantiate(laserPattern);
             laserArray[i].GetComponent<BossLaserScript>().player = player;
             laserArray[i].SetActive(false);
         }
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < 100; i++)
         {
             bulletArray[i] = Instantiate(bulletPattern);
             bulletArray[i].SetActive(false);
         }
-        CurrentHealth = 15f;
+        CurrentHealth = 50f;
+        laserQueue = 0;
+        laserTargetCooldown = 0;
     }
 
     void Update()
@@ -50,6 +54,13 @@ public class BossScript : MonoBehaviour
         attackCooldown -= Time.deltaTime;
         if (activated)
         {
+            laserTargetCooldown -= Time.deltaTime;
+            if (laserQueue >= 1 && laserTargetCooldown <= 0f)
+            {
+                LaserTarget();
+                laserQueue--;
+                laserTargetCooldown = 1f;
+            }
             if (attackCooldown <= 0)
             {
                 if (phase == 1)
@@ -58,12 +69,14 @@ public class BossScript : MonoBehaviour
                     if (attackChoice == 0)
                     {
 
-                        LaserTarget();
+                        laserQueue += 3;
+                        attackCooldown = 2f;
 
                     }
                     else
                     {
                         LaserRows(1.1f);
+                        attackCooldown = 2f;
                     }
                 }
                 else
@@ -72,24 +85,32 @@ public class BossScript : MonoBehaviour
                     switch (attackChoice)
                     {
                         case 0:
-                            LaserTarget();
-                            Invoke("LaserTarget", 0.7f);
-                            Invoke("LaserTarget", 1.4f);
+                            laserQueue += 3;
+                            attackCooldown = 2.5f;
                             break;
                         case 1:
-                            LaserRows(2f);
+                            LaserRows(2.1f);
+                            attackCooldown = 2.75f;
                             break;
                         case 2:
                             BulletCircle();
+                            attackCooldown = 3.2f;
                             break;
                         case 3:
-                            LaserSpin(5, true);
+                            if (Random.Range(0, 2) == 0)
+                            {
+                                LaserSpin(5, true);
+                            }
+                            else
+                            {
+                                LaserSpin(5, false);
+                            }
+                            attackCooldown = 3.5f;
                             break;
                         default:
                             break;
                     }
                 }
-                attackCooldown = 4f;
             }
         }
 
@@ -106,7 +127,8 @@ public class BossScript : MonoBehaviour
             {
                 phase = 2;
                 NextPhase();
-                attackCooldown = 7f;
+                attackCooldown = 5f;
+                laserQueue = 0;
             }
             else
             {
@@ -128,7 +150,7 @@ public class BossScript : MonoBehaviour
         {
             laserArray[i].SetActive(false);
         }
-        CurrentHealth = 50;
+        CurrentHealth = 100f;
     }
 
     private void LaserTarget()
@@ -153,16 +175,25 @@ public class BossScript : MonoBehaviour
 
     private void LaserRows(float width)
     {
-        for (int i = 1; i < 7; i += 2)
+        int index = 0;
+        for (int i = 0; i < laserArray.Length; i++)
+        {
+            if (laserArray[i].activeSelf == false)
+            {
+                index = i;
+                break;
+            }
+        }   
+        for (int i = 1+index; i < index+7; i += 2)
         {
             laserArray[i].transform.eulerAngles = new Vector3(0, 0, 0);
-            laserArray[i].transform.position = new Vector3(transform.position.x - width * i, transform.position.y - 30f, 0);
+            laserArray[i].transform.position = new Vector3(transform.position.x - width * (i-index), transform.position.y - 30f, 0);
             laserArray[i].GetComponent<BossLaserScript>().duration = 0.6f;
             laserArray[i].GetComponent<BossLaserScript>().spin = false;
             laserArray[i].SetActive(true);
 
             laserArray[i + 1].transform.eulerAngles = new Vector3(0, 0, 0);
-            laserArray[i + 1].transform.position = new Vector3(transform.position.x + width * i, transform.position.y - 30f, 0);
+            laserArray[i + 1].transform.position = new Vector3(transform.position.x + width * (i-index), transform.position.y - 30f, 0);
             laserArray[i + 1].GetComponent<BossLaserScript>().duration = 0.6f;
             laserArray[i + 1].GetComponent<BossLaserScript>().spin = false;
             laserArray[i + 1].SetActive(true);
@@ -170,6 +201,7 @@ public class BossScript : MonoBehaviour
     }
     private void BulletCircle()
     {
+        
         for (int i = 0; i < 45; i++)
         {
             float angle = i * 8f;
